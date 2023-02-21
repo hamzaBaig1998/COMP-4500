a4aefb5d29b057dec8ff2a2ae00a917b0143271e
Hamza Baig <39631430+hamzaBaig1998@users.noreply.github.com>
2023-02-21 10:59:04 -0500

    website updated

---
+++ website/assets/scripts/script.js
@@ -0,0 +1,25 @@
+const credits = document.getElementById("end-credits");
+const creditList = credits.querySelector("ul");
+const creditsHeight = credits.offsetHeight;
+const speed = 30; // Adjust this value to change the speed of the animation
+
+function resetCredits() {
+  creditList.style.transform = `translateY(${creditsHeight}px)`;
+  creditList.style.transition = `transform 0s`;
+}
+
+function animateCredits() {
+  creditList.style.transform = `translateY(-${creditsHeight}px)`;
+  creditList.style.transition = `transform ${creditsHeight / speed}s linear`;
+  setTimeout(() => {
+    resetCredits();
+  }, (creditsHeight / speed) * 1000);
+}
+
+animateCredits();
+// Repeat function
+setInterval(() => {
+  animateCredits();
+}, (creditsHeight / speed) * 1000 + 500);
+
+// timeline
